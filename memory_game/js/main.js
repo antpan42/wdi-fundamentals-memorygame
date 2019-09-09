40c0d5c4e4d58d2a2ec43752bd5fcdaf0190e9bf
Antpan42 <antpan42@gmail.com>
2019-09-08 23:55:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,19 +3,22 @@ console.log("Up and running!");
 
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User Flipped queen");
-const cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped king");
+function flipCard(cardId) {
+	const cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User Flipped queen");
+	const cardTwo = cards[3];
+	cardsInPlay.push(cardTwo);
+	console.log("User Flipped king");
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0]===cardsInPlay[3]) { 
 		alert("You Found A Match!")}
-		 else { alert("Sorry Try Again.")
+		 else { alert("Sorry Try Again.");}
+		 	console.log("User flipped" + cards[cardId]);
+		 		cardsInPlay.push(cards[cardId]);
 
 	}
-	
-
 }
+flipCard(0);
+flipCard(2);
 
